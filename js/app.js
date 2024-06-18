67f6a8da366e2efff8e16ce94da025238e7e78f4
Rafael Mainieri <rafasolmainieri@gmail.com>
2024-06-18 09:27:59 -0300

    criar functions e calcular valor do produto

---
+++ js/app.js
@@ -0,0 +1,21 @@
+function adicionar() {
+let produto = document.getElementById('produto').value;
+let nomeProduto = produto.split('-')[0];
+let valorUnitario = produto.split('R$')[1];
+
+let quantidade = document.getElementById('quantidade').value;
+
+//alert(nomeProduto);
+//alert(valorUnitario);
+//alert(quantidade.value)
+
+let preco = quantidade * valorUnitario;
+
+
+//alert(preco);
+
+}
+
+function limpar() {
+
+}
