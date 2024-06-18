f1872584a34876328d8387163aeee3db6f990253
Rafael Mainieri <rafasolmainieri@gmail.com>
2024-06-18 10:32:29 -0300

    calcular valor total e botao limpar

--- js/app.js
+++ js/app.js
@@ -1,24 +1,29 @@
+let totalGeral = 0;
+limpar();
+
 function adicionar() {
-let produto = document.getElementById('produto').value;
-let nomeProduto = produto.split('-')[0];
-let valorUnitario = produto.split('R$')[1];
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
 
-let quantidade = document.getElementById('quantidade').value;
+    let quantidade = document.getElementById('quantidade').value;
 
-//alert(nomeProduto);
-//alert(valorUnitario);
-//alert(quantidade.value)
+    let preco = quantidade * valorUnitario;
 
-let preco = quantidade * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML += `<section class="carrinho__produtos__produto">
+            <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+            </section>`;
 
-let carrinho = document.getElementById('lista-produtos');
-carrinho.innerHTML += `<section class="carrinho__produtos__produto">
-          <span class="texto-azul">${quantidade}</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
-        </section>`;
-//alert(preco);
+    totalGeral += preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`;
 
+    document.getElementById('quantidade').value = '';
 }
 
 function limpar() {
-
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0';    
 }
